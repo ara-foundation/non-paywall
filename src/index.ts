76ed2c5f1a9b8fd4518252210fa2160f667db8a0
Medet Ahmetson <milayter@gmail.com>
2024-04-08 11:39:53 +0700

    Use blockchain simulator

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,20 @@
 import * as url from "url";
 import express, { Express, Request, Response } from "express";
-import fs from "fs";
 import cors from "cors";
+import got from 'got';
 
-type PaymentInfo = {
-    endTime: number;
-    value: number;
-    duration?: 3600;
+const issueId: string = "66130f845b7028e14683c632";
+const implementationId: number = 1;
+const blockchainRpcUrl: string = "https://blockchain-sim-116ad6544f58.herokuapp.com";
+
+let subscribed = async function(userId: string): Promise<boolean> {
+    let url: string = `${blockchainRpcUrl}/eval_subscribed?issueId=${issueId}&implementationId=${implementationId}&userId=${userId}`;
+
+    console.log('Got: ' + url);
+    const data = await got(url).json();
+    return data as boolean;
 }
 
-type PaymentInfos = {
-    [key: string]: PaymentInfo
-}
-
-let paymentInfos: PaymentInfos = {};
-
-if (fs.existsSync("./payments.json")) {
-    console.log("File exists, let's read it");
-    let opened = fs.openSync("./payments.json", "rs+");
-    var file = fs.readFileSync(opened);
-    console.log(file.toString());
-    paymentInfos = JSON.parse(file.toString()) as PaymentInfos;
-    fs.closeSync(opened);
-} else {
-    console.log("File don't exist, let's create it");
-    paymentInfos = {"no_user": {
-            endTime: 123,
-            value: 5,
-            duration: 3600
-    }};
-
-    fs.writeFileSync("./payments.json", JSON.stringify(paymentInfos));
-}
-
-console.log("Opened file", paymentInfos);
-
-function getRandomInt(max: number) {
-    return Math.floor(Math.random() * max);
-}
-
-// 5.
-// define message passing between implementation and Maydan:
-// account.identify() -> sign the account
-// account.paymentRequired() -> ask user to pay
-// ??? how to identify the bot: we pass the impl and issue every time. to the client.
-// we also have an implementation that tests the implementation.
-
-// 1
-// in the constructor, we have a message system that sends an error to the client.
-// if message "402" received, then SenSever appears next to the web page.
-// he asks for the payment and then creates a transaction.
-// the created transaction is added to the file.
 
 const nonPaywalls = [
     "ft.com",
@@ -86,30 +50,14 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
 });
 
-app.get("/pay/:user", (req: Request, res: Response) => {
-    const user = req.params.user;
-    console.log("Hello, paying customer " + user);
+app.get("/validate/:userId/:url", async (req: Request, res: Response) => {
+    const userId = req.params.userId;
+    console.log("Hello, " + userId);
 
-    const now = Math.floor(new Date().getTime() / 1000);
-    const paymentInfo: PaymentInfo = {
-        endTime: now + 300, // pay every 5 minutes
-        value: 5.6,
-    }
+    let isSubscribed = await subscribed(userId);
+    console.log('Are you subscribed to this implementation: ' + isSubscribed);
 
-    paymentInfos[user] = paymentInfo;
-
-    fs.writeFileSync("./payments.json", JSON.stringify(paymentInfos));
-
-    res.json(paymentInfo);
-});
-
-app.get("/validate/:user/:url", async (req: Request, res: Response) => {
-    const user = req.params.user;
-    console.log("Hello, " + user);
-
-    const now = Math.floor(new Date().getTime() / 1000);
-
-    if (!paymentInfos.hasOwnProperty(user) || paymentInfos[user].endTime <= now) {
+    if (!isSubscribed) {
         return res.status(402).json({
             nonPaywall: false,
             passUrl: ""
